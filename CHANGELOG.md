c84f74673013291658531062cc6cddd493283f3d
danse <f.occhipinti@gmail.com>
2014-12-15 17:12:16 +0100

    provide logic to safely convert to admin divisions, closes #2 and sprintly 890

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## [1.2.0] - 2014-12-15
+### Added
+- selected location `getAdminDivisions` and `setAdminDivisions` methods
+
 ## [1.1.0] - 2014-12-15
 ### Added
 - `locations.decode` method
--- dist/ehealthlocations.js
+++ dist/ehealthlocations.js
@@ -108,13 +108,14 @@ angular.module('eHealth.locations.services')
 'use strict';
 
 angular.module('eHealth.locations.services')
-  .factory('selectedLocationFactory', function (locations) {
+  .factory('selectedLocationFactory', function (locations, $log) {
     function create(options) {
       var options = options || {},
           hasAllItem = options.hasAllItem,
           locationsData = options.locationsData || locations,
           original = angular.copy(locationsData),
-          levels = [];
+          levels = [],
+          location;
 
       function reset(index) {
         var level = angular.copy(original[index]);
@@ -193,7 +194,7 @@ angular.module('eHealth.locations.services')
         reset(index);
       });
 
-      return {
+      location = {
         levels: levels,
         select: function(level, id) {
           levels[level].selectById(id);
@@ -221,8 +222,43 @@ angular.module('eHealth.locations.services')
             }
           }
           return equal;
+        },
+        setAdminDivisions: function(obj) {
+          // selected locations have the constraint that lower level
+          // locations must be child of the higher level ones, while
+          // in the object we might have inconsistencies. starting
+          // from the bottom, pick the most specific regional info if
+          // available and ignore the rest. this code assumes that we
+          // have no more than 5 administrative levels
+          var toBeSelected = true;
+          [5, 4, 3, 2, 1].forEach(function(n) {
+            if (toBeSelected) {
+              var i = n - 1,
+                  id = obj['adminDivision'+n];
+              if (id) {
+                try {
+                  location.select(i, id);
+                  toBeSelected = false;
+                } catch (error) {
+                  $log.error(error);
+                }
+              }
+            }
+          });
+          return location;
+        },
+        getAdminDivisions: function(adminDivisions) {
+          adminDivisions = adminDivisions || {};
+          levels.forEach(function(level, index) {
+            if (level.selected) {
+              var key = 'adminDivision'+(index+1);
+              adminDivisions[key] = level.selected.id;
+            }
+          });
+          return angular.copy(adminDivisions);
         }
       };
+      return location;
     }
     return create;
   });
