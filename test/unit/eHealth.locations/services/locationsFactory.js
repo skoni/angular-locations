73a14a31778ba8ae630b1c1cba238230e57d6f05
Patricia Garcia <pat@patriciagarcia.me>
2014-12-22 18:45:03 +0100

    Add tests

--- test/unit/eHealth.locations/services/locationsFactory.js
+++ test/unit/eHealth.locations/services/locationsFactory.js
@@ -7,12 +7,12 @@
 
 describe('the locationsFactory service', function(){
   var locationsFactory;
-  
+
   beforeEach(module('eHealth.locations'));
   beforeEach(inject(function(_locationsFactory_) {
     locationsFactory = _locationsFactory_;
   }));
-  ['ml', 'lr', 'gn', 'gin', 'sl'].forEach(function(code) {
+  ['ml', 'lr', 'gn', 'gin', 'sl', 'mg'].forEach(function(code) {
     it('provides locations for '+code, function(){
       expect(locationsFactory(code)).toBeDefined();
     });
